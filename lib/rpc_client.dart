4c7c8a6a916fc1e1422bfab5549690a76acd50c1
Roman Volovoy <romanvolovoy@gmail.com>
2025-04-08 23:00:14 +0200

    rpc_client exports

---
+++ lib/rpc_client.dart
@@ -0,0 +1,8 @@
+library;
+
+export 'src/rpc_client/rpc_client.dart';
+export 'src/rpc_client/exceptions.dart';
+export 'src/rpc_client/transport.dart';
+
+//implementations
+export 'src/rpc_client/transport/http.dart';
