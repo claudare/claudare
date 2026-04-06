c1e6b7908d9d33b6e1c49c848de296cffebce403
Roman <romanvolovoy@gmail.com>
2026-04-06 19:59:39 +0200

    upgrade packages

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import restart_app
-import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   RestartAppPlugin.register(with: registry.registrar(forPlugin: "RestartAppPlugin"))
-  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
