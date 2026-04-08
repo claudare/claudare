543fc86efe8105d417b5e8b67f55ba2e7bac2980
Roman <romanvolovoy@gmail.com>
2026-04-08 14:52:23 +0200

    bulk deleting notes

--- lib/screens/home/note_list_controller.dart
+++ lib/screens/home/note_list_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:notes_app_v0/command/delete_note.dart';
 import 'package:notes_app_v0/model/note_data.dart';
 import 'package:notes_app_v0/repo/note/note_read_model.dart';
 import 'package:notes_app_v0/runtime/notes_runtime.dart';
@@ -42,5 +43,23 @@ class NoteListController extends ChangeNotifier {
       _isLoading = false;
       notifyListeners();
     }
+  }
+
+  Future<void> deleteNotes(List<String> noteIds) async {
+    final promises = noteIds.map(
+      (noteId) => notesRuntime.commands.deleteNote.runResult(
+        DeleteNoteInput(noteId: noteId),
+      ),
+    );
+
+    final results = await Future.wait(promises);
+
+    for (final result in results) {
+      if (!result.success) {
+        print('Failed to delete note: $result');
+      }
+    }
+
+    await reloadNotes();
   }
 }
